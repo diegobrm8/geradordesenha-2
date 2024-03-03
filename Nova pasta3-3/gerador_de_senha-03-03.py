aa2e789c125de4b3312448760001829e8bd4439e
diegobrm8 <111235814+diegobrm8@users.noreply.github.com>
2024-03-03 09:59:21 -0300

    Removendo todos os arquivos, exceto 'Nova pasta3-3'

--- Nova pasta3-3/gerador_de_senha-03-03.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import string as s
-from secrets import SystemRandom as Sr
-
-random = secrets.SystemRandom()
-
-print(''.join(Sr().choices(s.ascii_letters + s.digits + s.punctuation, k=8)))
